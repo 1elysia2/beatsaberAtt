e6fbb5e901d616f36798182a1077e448397ee014
1elysia2 <1768842823@qq,com>
2026-04-13 15:50:09 +0800

    a

--- Scripts/Judgement/JudgementSetting.cs
+++
@@ -1,7 +0,0 @@
-[System.Serializable]
-public class JudgmentSettings
-{
-    public float perfectTimeRange = 0.05f; // Perfect判定时间范围（秒）
-    public float goodTimeRange = 0.1f;     // Good判定时间范围（秒）
-    public float hitDistance = 2.0f;       // 判定平面前后的空间范围（米）
-}
